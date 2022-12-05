0d7bb7163111b69f215b143744f79c02e867f399
Corey Gilbert <coreyraygilbert@gmail.com>
2022-12-04 20:53:49 -0500

    Added get&set

--- Program.cs
+++ Program.cs
@@ -26,4 +26,58 @@ public class cardHolder
     {
         return pin;
     }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName = lastName; 
+    }
+
+    public double getBalance()
+    {
+        return balance = balance;   
+    }
+
+    public void setNum(String newCardNum)
+    {
+        cardNum = newCardNum;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;   
+    }
+
+    public void setFirstName(String newFirstName)
+    {
+        firstName = newFirstName;
+    }
+
+    public void setLastName(String newLastName)
+    {
+        lastName = newLastName;
+    }
+
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+
+    public static void Main(String[] args)
+    {
+        void printOptions()
+        {
+            Console.WriteLine("Please choose from one of the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+    }
+
+
 }
