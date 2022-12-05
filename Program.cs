f887b6d9bc111656187d0c1920caa42bd7cd6dc5
Corey Gilbert <coreyraygilbert@gmail.com>
2022-12-04 20:43:18 -0500

    First Push

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class cardHolder
+{
+    String cardNum;
+    int pin;
+    String firstName;
+    String lastName;
+    double balance;
+
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    public String getNum()
+    {
+        return cardNum;
+    }
+
+    public int getPin()
+    {
+        return pin;
+    }
+}
